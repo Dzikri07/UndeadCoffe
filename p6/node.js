fdac33f825d22f863d35f8ec9189e117a2e10a58
Dzikr <07.dzikri@gmail.com>
2025-04-15 18:41:15 +0700

    perubahan font dan tambahan reaction time

---
+++ p6/node.js
@@ -0,0 +1,38 @@
+// alert("apalah");
+// nama = prompt("siapa anda ?");
+
+// alert("Selamat datang, " + nama);
+
+// jawab = confirm("pilih ok apa cancel");
+
+// if (jawab) {
+//     alert("anda memilih OK");
+// } else {
+//     alert("anda memilih CANCEL");
+// }
+
+// document.writeln("saya belajar... <br>");
+// document.writeln("<h1>saya</h1>");
+
+
+// console.log("tes satu " + jawab);
+
+
+// namaLengkap = "Dzikri Muhammad Akbar"
+// document.getElementById('nama').innerHTML=namaLengkap
+
+
+let angka = 50
+let nama = "dzikri"
+    nama = "kag"
+let benar = true
+let undfn;
+let n1 = null;
+let bilangan = [1,2,3,4,5,6,7,8,9,10,11]
+let buah = ["anggur", "jeruk", "mangga"]
+
+document.writeln(benar)
+document.writeln(undfn)
+document.writeln("<br>"+ n1)
+document.writeln("<br>"+ nama+3)
+document.write( "<br>" + bilangan[5])
